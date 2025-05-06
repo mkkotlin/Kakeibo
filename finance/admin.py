fb966986d406fd2f1a77271c2f73da682f135811
mkkotlin <123.ultron.1234@gmail.com>
2025-05-06 15:28:12 +0530

    models and database added

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from finance.models import Expense, Income
 
 # Register your models here.
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('category','amount','date','notes')
+    list_filter = ('category','date')
+    search_fields = ('notes',)
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('source','date','notes')
+    list_filter = ('source','date')
+    search_fields = ('source','notes')
--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length = 100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Expense(models.Model):
+    
+    CATEGORY_CHOICES = [
+        ('food','Food'),
+        ('transport','Transport'),
+        ('bills','Bills'),
+        ('entertainment','Entertainment'),
+        ('health','Health'),
+        ('others','Others'),
+    ]
+
+    category = models.CharField(max_length = 20, choices = CATEGORY_CHOICES)
+    amount = models.DecimalField(max_digits = 10, decimal_places = 2)
+    date = models.DateField(auto_now_add = True)
+    notes = models.TextField(blank = True)
+
+    def __str__(self):
+        return f"{self.category} - $ {self.amount} on {self.date}"
+
+class Income(models.Model):
+    source = models.CharField(max_length = 100)
+    amount = models.DecimalField(max_digits = 10, decimal_places = 2)
+    date = models.DateField(auto_now_add = True)
+    notes = models.TextField(blank = True)
+
+    def __str__(self):
+        return f"{self.source} - $ {self.amount} on {self.date}"
