441cffadf2a42f581da4a722015daf4d8209d1ac
mkkotlin <123.ultron.1234@gmail.com>
2025-05-06 15:42:22 +0530

    fixed choices db error

--- finance/models.py
+++ finance/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length = 100, unique=True)
+# #
+# class Category(models.Model):
+#     name = models.CharField(max_length = 100, unique=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 class Expense(models.Model):
     
