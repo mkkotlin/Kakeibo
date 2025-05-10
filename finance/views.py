8a168f8d543d546ffa4ef2497314865e1cfbb4b9
mkkotlin <123.ultron.1234@gmail.com>
2025-05-10 13:10:03 +0530

    cors updatde, url fixes

--- finance/views.py
+++ finance/views.py
@@ -4,6 +4,7 @@ from finance.models import Expense, Income, Category
 from django.core.serializers import serialize
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
 
@@ -12,6 +13,7 @@ def finance(request):
     return HttpResponse("http://127.0.0.1:8000/api/income/  <br>    http://127.0.0.1:8000/api/expenses/")
 
 @csrf_exempt
+@require_http_methods(["GET","POST"])
 def expense_list(request):
     if request.method == "GET":
         # data = Expense.objects.select_related('category').all()
@@ -30,7 +32,7 @@ def expense_list(request):
         category = request.GET.get('category')
         date = request.GET.get('date')
         expenses = Expense.objects.select_related('category').all()
-        notes = request.GET.get('notes')
+        notes = request.GET.get('notes','').strip()
         
 
         if category:
@@ -48,12 +50,15 @@ def expense_list(request):
 
         min_amount = request.GET.get('min_amount')
         max_amount = request.GET.get('max_amount')
-        if min_amount:
-            expenses = expenses.filter(amount__gte=min_amount)
-        if max_amount:
-            expenses = expenses.filter(amount__lte=max_amount)
-        if min_amount and max_amount:
-            expenses = expenses.filter(amount__range=[min_amount, max_amount])
+        try:
+            if min_amount and max_amount:
+                expenses = expenses.filter(amount__range=[min_amount, max_amount])
+            elif min_amount:
+                expenses = expenses.filter(amount__gte=min_amount)
+            elif max_amount:
+                expenses = expenses.filter(amount__lte=max_amount)
+        except  ValueError:
+            return JsonResponse({'error':'amount must be number'}, status=400)
 
 
         result = [
@@ -75,19 +80,44 @@ def expense_list(request):
             category_id = data.get("category")
             category = Category.objects.get(id = category_id)
             expense = Expense.objects.create(
-                # category = category,
-                category = Category.objects.get(id = data.get("category")),
+                category = category,
+                # category = Category.objects.get(id = data.get("category")),
                 amount = data.get('amount'),
                 notes = data.get('notes', '')
             )
             return JsonResponse({'message':'Expense created successfully', 'id': expense.id}, status=201)
         except Exception as e:
-            return JsonResponse({'error':str(e)}, status=400)
+            # return JsonResponse({'error':str(e)}, status=400)
+            return JsonResponse({'error':'Invalid category ID'},status = 400)
 
 @csrf_exempt
+@require_http_methods(["GET","POST"])
 def income_list(request):
     if request.method == "GET":
-        data = Income.objects.all()
+        incomes = Income.objects.all()
+
+# filters
+        source = request.GET.get('source','').strip()
+        if source:
+            incomes = incomes.filter(source__icontains = source)
+        min_amount = request.GET.get('min_amount')
+        max_amount = request.GET.get('max_amount')
+        try:
+            if min_amount and max_amount:
+                incomes = incomes.filter(amount__range=[min_amount,max_amount])
+            elif min_amount:
+                incomes = incomes.filter(amount__gte=min_amount)
+            elif max_amount:
+                incomes = incomes.filter(amount__lte=max_amount)
+        except  ValueError:
+            return JsonResponse({'error':'amount must be number'}, status=400)
+        date = request.GET.get('date')
+        if date:
+            incomes = incomes.filter(date = date)
+        notes = request.GET.get('notes')
+        if notes:
+            incomes = incomes.filter(notes__icontains = notes)
+
         result = [
             {
                 "id":income.id,
@@ -96,7 +126,7 @@ def income_list(request):
                 "date": income.date,
                 "notes":income.notes
             }
-            for income in data
+            for income in incomes
         ]
         return JsonResponse(result, safe = False, json_dumps_params = {"indent":2})
     elif request.method == "POST":
